034f98317513d5b0d7f9698edf99b25f747548d1
jardim3162 <joao.2020316204@aluno.iffar.edu.br>
2024-10-31 16:50:21 -0300

    atualizado o banco de dados

---
+++ tcc (1).sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 31-Out-2024 às 19:39
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+CREATE TABLE IF NOT EXISTS `material` (
+  `id_material` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `estoque` int NOT NULL,
+  `descricao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_material`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `material`
+--
+
+INSERT INTO `material` (`id_material`, `nome`, `estoque`, `descricao`) VALUES
+(3, 'Sabão Barras', 8, 'Sabão Barra, marca \"Dove\"'),
+(9, 'Detergente', 10, 'detergente, marca ipan, cor laranja, 700ml'),
+(11, 'Sabão em Pó', 10, 'Sabão em Pó, marca OMO'),
+(15, 'papel sulfite', 6, 'papel sulfite, a4, cor pardo'),
+(16, 'papel toalha', 4, 'papel toalha, tamanho 20ml, cor branca');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `id_pedido` int NOT NULL AUTO_INCREMENT,
+  `data_pedido` date NOT NULL,
+  `quantidade` int NOT NULL,
+  PRIMARY KEY (`id_pedido`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `recuperar-senha`
+--
+
+DROP TABLE IF EXISTS `recuperar-senha`;
+CREATE TABLE IF NOT EXISTS `recuperar-senha` (
+  `email` varchar(255) NOT NULL,
+  `data_criacao` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome`, `email`, `senha`) VALUES
+(1, 'Thiago', 'thiago.krug@iffarroupilha.edu.br', '123'),
+(5, 'João', 'joao@mail.com', '123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
