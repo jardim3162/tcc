343f8431e26ab3efaad3d9e0588662ac837afd74
jardim3162 <joao.2020316204@aluno.iffar.edu.br>
2025-01-27 11:29:50 -0300

    atualizado navbar

--- Telainicial.php
+++ Telainicial.php
@@ -10,28 +10,26 @@ require_once "funcoes.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tela inicial</title>
-  <link rel="stylesheet" href="css/styles.css?nocache=<?= rand() ?>">
+  <link rel="stylesheet" href="css/styles.css?">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
-<?php include "navadm.php"; ?>
+<?php include "navadm.php" ?>
 <body id="telainicial">
-  <div class="container" style="margin-top: 10%;">
+  <div class="container">
     <br>
-    <div class="d-flex justify-content-between align-items-center mb-4">
+    <div class="d-flex justify-content-between align-items-center mb-4" style="margin-top: 15%;">
       <h2>Estoque de Materiais</h2>
       <form method="GET" class="form-inline">
         <div class="input-group">
-          <input type="text" name="pesquisa" class="form-control" placeholder="Pesquisar material" 
-                 value="<?=$termoPesquisa; ?>">
+          <input type="text" name="pesquisa" class="form-control" placeholder="Pesquisar material" value="<?=$termoPesquisa; ?>">
           <div class="input-group-append">
             <button type="submit" class="btn btn-primary">Pesquisar</button>
           </div>
         </div>
       </form>
     </div>
-
-    <div class="table-responsive" style="max-height: 800px; overflow-y: auto; margin: 0 auto; width: 100%;">
+    <div class="table-responsive">
       <table class="table table-bordered table-hover text-center">
         <thead class="thead-dark">
           <tr>
@@ -71,5 +69,6 @@ require_once "funcoes.php";
       <a href="form-material.php" class="btn btn-primary">Cadastrar Material</a>
     </div>
   </div>
+
 </body>
 </html>
